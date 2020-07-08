0f78a292ff9723248042b0190bba86113da01808
Jessica Lane Ramos <41808261+JessicaRamoslc@users.noreply.github.com>
2020-07-08 06:28:02 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,26 +21,26 @@ var x = setInterval(function() {
   document.getElementById("demo").innerHTML = `
   <div style="display: flex; padding: 20px">
 <div id="floatContainer" style="
-  position: relative; margin-left: -15px;  margin-right:23px; border:black 2px solid; padding: 15px;">
+  position: relative; margin-left: -15px;  margin-right:23px; border:black 2px solid; padding: 10px;">
   <p">${days}</p>
   <p style="border: none;
   outline: 0; margin-top:  -20px">dia</p>
 </div>
 <div id="floatContainer" style="
-  position: relative; margin-right:23px; border:black 2px solid; padding: 15px; ">
+  position: relative; margin-right:23px; border:black 2px solid; padding: 10px; ">
   <p>${hours}</p>
   
   <p style="border: none;
   outline: 0; margin-top:  -20px">h</p>
 </div>
 <div id="floatContainer" style="
-  position: relative; margin-right:23px; border:black 2px solid; padding: 15px; ">
+  position: relative; margin-right:23px; border:black 2px solid; padding: 10px; ">
   <p>${minutes}</p>
   <p style="border: none;
   outline: 0; margin-top:  -20px"> m </p>
 </div>
 <div id="floatContainer" style="
-  position: relative; margin-right:23px; border:black 2px solid; padding: 15px; ">
+  position: relative; margin-right:23px; border:black 2px solid; padding: 10px; ">
   <p>${seconds}</p>
   <p style="border: none;
   outline: 0; margin-top:  -20px">s</p>
