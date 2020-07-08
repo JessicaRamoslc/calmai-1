2b72aced95b2f0e49f1d419badca325b79a7d6a9
Marcos <marcos.paulo.rodrigues@hotmail.com>
2020-07-08 11:12:19 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,12 +67,3 @@ document.addEventListener('mouseleave', function () {
   }
 })
 
-//Animação link
-
-var $doc = $('html, body');
-$('a').click(function() {
-    $doc.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});
